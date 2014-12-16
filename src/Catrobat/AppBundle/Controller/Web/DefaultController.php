9b7d136f3a0f74fdcc481709c95dc5ff48f9a09c
Melket <saracevic@student.tugraz.at>
2014-12-16 15:45:59 +0100

    Details Page Update

---
+++ src/Catrobat/AppBundle/Controller/Web/DefaultController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Catrobat\AppBundle\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class DefaultController extends Controller
+{
+  public function headerAction()
+  {
+    return $this->get("templating")->renderResponse(':Default:header.html.twig');
+  }
+
+  public function headerLogoAction()
+  {
+    return $this->get("templating")->renderResponse(':Default:headerLogo.html.twig');
+  }
+
+  public function footerAction()
+  {
+    return $this->get("templating")->renderResponse(':Default:footer.html.twig');
+  }
+
+  /**
+   * @Route("/", name="catrobat_web_index")
+   * @Method({"GET"})
+   */
+  public function indexAction()
+  {
+    return $this->get("templating")->renderResponse('::index.html.twig');
+  }
+
+  /**
+   * @Route("/program/{id}", name="catrobat_web_program", requirements={"id":"\d+"})
+   * @Route("/details/{id}", name="catrobat_web_detail", requirements={"id":"\d+"})
+   * @Method({"GET"})
+   */
+  public function programAction($id)
+  {
+    $program = $this->get("programmanager")->find($id);
+    $elapsed_time = $this->get("elapsedtime");
+    $screenshot_repository = $this->get("screenshotrepository");
+
+    if (!$program) {
+      throw $this->createNotFoundException('Unable to find Project entity.');
+    }
+
+    $program->setViews($program->getViews() + 1);
+    $entity_manager = $this->get("doctrine.orm.entity_manager");
+    $entity_manager->persist($program);
+    $entity_manager->flush();
+
+    return $this->get("templating")->renderResponse('::program.html.twig', array(
+        "program" => $program,
+        "elapsedTime" => $elapsed_time->getElapsedTime($program->getUploadedAt()->getTimestamp()),
+        "screenshot" => $screenshot_repository->getScreenshotWebPath($program->getId())
+    ));
+  }
+
+  /**
+   * @Route("/search/{q}", name="catrobat_web_search", requirements={"q":".+"})
+   * @Method({"GET"})
+   */
+  public function searchAction($q)
+  {
+    return $this->get("templating")->renderResponse('::search.html.twig', array("q" => $q));
+  }
+
+  /**
+   * @Route("/search/", name="catrobat_web_search_nothing")
+   * @Method({"GET"})
+   */
+  public function searchNothingAction()
+  {
+    return $this->get("templating")->renderResponse('::search.html.twig', array("q" => null));
+  }
+
+  /**
+   * @Route("/profile/{id}", name="catrobat_web_profile", requirements={"id":"\d+"})
+   * @Method({"GET"})
+   */
+  public function profileAction($id)
+  {
+    $profile = $id;
+
+    return $this->get("templating")->renderResponse('::profile.html.twig', array("profile" => $profile));
+  }
+
+  /**
+   * @Route("/termsOfUse", name="catrobat_web_termsOfUse")
+   * @Method({"GET"})
+   */
+  public function termsOfUseAction()
+  {
+    return $this->get("templating")->renderResponse('::termsOfUse.html.twig');
+  }
+
+  /**
+   * @Route("/licenseToPlay", name="catrobat_web_licenseToPlay")
+   * @Method({"GET"})
+   */
+  public function licenseToPlayAction()
+  {
+    return $this->get("templating")->renderResponse('::licenseToPlay.html.twig');
+  }
+}
